44127dffc94dc054ee7c057fcdc5aacc3675a1bf
AcnoZed <Me.ouss.am@outlook.de>
2020-04-03 22:43:43 +0200

    Initialize Project

---
+++ src/main/java/acno/heapSortVisual/HeapSortVisualApplication.java
@@ -0,0 +1,13 @@
+package acno.heapSortVisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeapSortVisualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeapSortVisualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/acno/heapSortVisual/HeapSortVisualApplicationTests.java
@@ -0,0 +1,13 @@
+package acno.heapSortVisual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeapSortVisualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
